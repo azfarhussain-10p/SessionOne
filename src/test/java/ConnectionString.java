2a3724ae0d3941169529e828e367c9227be2ce0a
Azfar Hussain <ahmzee@live.com>
2024-03-19 15:34:30 +0500

    Initialize DesiredCapabilities

---
+++ src/test/java/ConnectionString.java
@@ -0,0 +1,36 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ConnectionString {
+
+    AppiumDriver driver;
+
+    @BeforeTest
+    public void initializer() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("app", System.getProperty("user.dir") + "/app/ApiDemos-debug.apk");
+
+        driver = new AndroidDriver(new URL("http://localhost:4723"), capabilities);
+        System.out.println("Opening Appium Server");
+    }
+
+    @Test
+    public void sampleTest(){
+        System.out.println("This is my First Sample Test");
+    }
+
+    @AfterTest
+    public void tearDown(){
+        driver.quit();
+    }
+}
